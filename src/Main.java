b41ac178e1907739b878dc2f49b087580fa16a4a
A-Dobrovolsky <a.dobrovolsky4@gmail.com>
2017-12-17 19:56:51 +0300

    switch to branch branch2

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Second commit");
         System.out.println("Next Commit");
         System.out.println("Kept you waiting, huh?");
+        System.out.println("branch2");
     }
 }
